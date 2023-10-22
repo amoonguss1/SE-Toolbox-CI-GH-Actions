3fa4e6e1de643a874e958945602b5d6738bac91d
adriel kee <adrielkee@gmail.com>
2023-10-22 21:10:50 +0800

    Edit stuff

--- user-service/test/vitest.config.ts
+++ user-service/test/vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test: {
-      include: ['./systemtest/*.test.ts'],
-      exclude: ['./test/**/*'],
-      threads: false
-    }
-  })
+  test: {
+    include: ['./test/test.ts'],
+    threads: false
+  }
+})
